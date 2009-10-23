60b525710c9cd3d688e2b133eeddf32ad33e319c
Ryan Heneise <ryan@artofmission.com>
2009-10-22 20:58:25 -0400

    Hacked Rails' aggregations to use any method (via :send) rather than relying on database attributes.

---
+++ lib/money/aggregations.rb
@@ -0,0 +1,30 @@
+# Rails reader_method uses read_attribute to get the data from a column. 
+# This doesn't let us use calculated columns or delegated methods. 
+# We overwrite read_attribute with send, which works with any available method. 
+
+module ActiveRecord
+  module Aggregations # :nodoc:
+    module ClassMethods
+      def reader_method(name, class_name, mapping, allow_nil, constructor)
+        module_eval do
+          define_method(name) do |*args|
+            force_reload = args.first || false
+            if (instance_variable_get("@#{name}").nil? || force_reload) && (!allow_nil || mapping.any? {|pair| !read_attribute(pair.first).nil? })
+              attrs = mapping.collect {|pair| send(pair.first)}
+              object = case constructor
+                when Symbol
+                  class_name.constantize.send(constructor, *attrs)
+                when Proc, Method
+                  constructor.call(*attrs)
+                else
+                  raise ArgumentError, 'Constructor must be a symbol denoting the constructor method to call or a Proc to be invoked.'
+                end
+              instance_variable_set("@#{name}", object)
+            end
+            instance_variable_get("@#{name}")
+          end
+        end
+      end
+    end
+  end
+end
